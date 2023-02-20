36ab2fb1188b66a017938c8acb2f287c3bdaa4cd
Logan Fasano <58754739+LFasano0129@users.noreply.github.com>
2023-02-20 01:22:26 -0500

    Final Commit
    Project complete.

---
+++ main.py
@@ -0,0 +1,8 @@
+# print statement to welcome the user.
+print("Hello, welcome to the Band Name Generator!")
+# input function to get the users city name
+city_name = input("What is the name of the city you grew up in?\n")
+# input function to get the users pet name
+pet_name = input("What is the name of a current or past pet of yours?\n")
+# print statement to present the users band name
+print("Your band name is " + city_name + " " + pet_name)
